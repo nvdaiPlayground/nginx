99e523591d939225ffa21ad3843b8bab6a5caaff
nvdaiPlayground <nvdai1506@playgroundvina.com>
2024-01-24 16:22:12 +0700

    init

---
+++ node-app/app.js
@@ -0,0 +1,14 @@
+// app.js
+const express = require("express");
+const app = express();
+const port = 1506;
+
+// Define a route
+app.get("/hello", (req, res) => {
+  res.send("Hello, this is a simple Express apppp!");
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
